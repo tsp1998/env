6a584444d3e0a705076ae0c4dbf27ac72247c163
Shubham Tandale <shubhamtandale@outlook.in>
2022-11-17 16:57:57 +0530

    added pnpm and removed yarn lock file and updated copy snippets file to support windows

--- .vscode/src/utils/copy-snippets.ts
+++ .vscode/src/utils/copy-snippets.ts
@@ -5,13 +5,20 @@ const localSnippetFolderPath = path.resolve(__dirname, '..', '..', 'snippets')
 let snippetFolderPath = ''
 const platform = os.platform();
 const homeDir = os.homedir()
-if (platform === 'linux') {
-  snippetFolderPath = `${homeDir}/.config/Code/User/snippets`
+
+switch (true) {
+  case platform.includes('linux'): {
+    snippetFolderPath = `${homeDir}/.config/Code/User/snippets`
+  } break;
+  case platform.includes('win'): {
+    snippetFolderPath = `${homeDir}/AppData/Roaming/Code/User/snippets`
+  } break;
 }
+
 if (!fs.existsSync(snippetFolderPath)) {
   fs.mkdirSync(snippetFolderPath)
 }
-const snippetFiles = fs.readdirSync(localSnippetFolderPath)
+const snippetFiles: string[] = fs.readdirSync(localSnippetFolderPath)
 snippetFiles.forEach(snippetFile => {
   const srcFilePath = path.join(localSnippetFolderPath, snippetFile)
   const destFilePath = path.join(snippetFolderPath, snippetFile)
